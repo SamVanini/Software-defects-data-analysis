5d7208b285178b9d7076dcd8e027bd7a20785025
Samantha Vanini <vaninisamantha@yahoo.it>
2026-03-02 21:51:09 +0100

    test: added unit test for load_raw_data function

--- tests/test_data_processing.py
+++ tests/test_data_processing.py
@@ -1,11 +1,38 @@
 import pytest
 from src.data_processing import load_raw_data
 
-def load_raw_data_not_existant_file_should_return_none():
-    ...
+def test_load_raw_data_not_existant_file_should_return_none():
+    # Arrange
+    non_existant_file = "notexistant.csv"
 
-def load_raw_data_not_existant_env_should_use_fallback():
-    ...
+    # Act
+    df = load_raw_data(non_existant_file)
 
-def load_raw_data_existant_file_should_return_dataframe():
-    ...
+    # Assert
+    assert df is None
+
+def test_load_raw_data_not_existant_env_should_use_fallback(mock_env_data_dir):
+    # Arrange
+    file_name = "sample.csv"
+
+    # Act
+    df = load_raw_data(file_name)
+
+    #Assert
+    assert df is None
+
+@pytest.fixture
+def mock_env_data_dir(monkeypatch):
+    monkeypatch.setenv("DATA_DIR", "notexistant")
+
+def test_load_raw_data_existant_file_should_return_dataframe():
+    # Arrange
+    file_name = "sample.csv"
+
+    # Act
+    df = load_raw_data(file_name)
+
+    # Assert
+    assert df is not None
+    assert len(df.columns) >= 6
+    assert len(df) > 0
